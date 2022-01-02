61067825060d13090a1b7c92ae6eb0f43e82fa44
Heeji Hyun <wlgmlgus@gmail.com>
2022-01-02 13:52:16 -0500

    Completed assignment

--- test/index-test.js
+++ test/index-test.js
@@ -1,7 +1,7 @@
 const { expect } = require("chai");
 const { name, height, message } = require("../index.js");
 
-/*
+
 describe('what-is-a-test', () => {
   describe('Name', () => {
     it('returns "Susan"', () => {
@@ -23,22 +23,3 @@ describe('what-is-a-test', () => {
     })
   })
 })
-*/
-describe("Name", () => {
-  it('returns "Susam"', () => {
-    expect(name).toEqual("Susan");
-  });
-});
-
-describe("Height", () => {
-  it("is less than 40", () => {
-    expect(height).toBeLessThan(40);
-  });
-});
-
-describe("Message", () => {
-  it("gives the name and height", () => {
-    expect(message).toInclude(name);
-    expect(message).toInclude(height);
-  });
-});
